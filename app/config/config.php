0ae0f334ce6612631e40efa020e9845d1d6e9307
Alvaro-Sargento <47075852+Alvaro-Sargento@users.noreply.github.com>
2022-11-09 01:09:40 +0200

    push de UI inicial

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'projectoLES');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/projectoLES/app');
+  // Site Name
+  define('SITENAME', 'projectoLES');
+  // App Version
+  define('APPVERSION', '1.0.0');
---
+++ app/views/inc/wellcome.php
@@ -0,0 +1,13 @@
+<!-- particles.js container -->
+<div id="particles-js">
+	<div class="row justify-content-center">
+    	<div class="row col-8">
+    		<h1>Olá, esse é o ficheiro de apresentação!</h1>
+    	</div>
+    </div>
+</div>
+
+<!-- scripts -->
+<script src="..assets/particles/particles.js"></script>
+<script src="..assets/particles/demo/js/app.js"></script>
+
